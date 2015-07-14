491c4665440fae83684d62611963687f709721ef
subtleq <sam.lanzo@gmail.com>
2015-07-13 20:59:28 -0700

    Update autohack.py
    flesh out some more functions

--- autohack.py
+++ autohack.py
@@ -2,32 +2,51 @@
 # program to auto hack nearby networks
 import threading
 import time
+import os
 
 # list of networks to hack
 networks = []
+
+# sleep timers for the threads between updates
 display_sleep = 1
-discovery_sleep = 100
+discovery_sleep = 120
 autohack_sleep = 10
 
 # update the command line display TODO
 def display_thread():
   while True:
-    # update the display TODO
+    # sleep until next update time
     time.sleep(display_sleep)
 
-# update the list of nearby networks TODO  
+# add information about found networks to the shared network list TODO
+def update_networks(found_networks, interface):
+  global networks
+  return 0
+
+# update the list of nearby networks
 def discovery_thread():
   while True:
-    # look for nearby networks TODO
+    # get the interfaces on this machine
+    interfaces = [x.split(' ')[0] for x in os.popen('ifconfig -s', 'r').read().split('\n')[2:]][:-1]
+
+    # get a list of current networks
+    for interface in interfaces:
+      # get a list of networks seen by the interface
+      interface_networks = os.popen('iwlist ' + interface + ' scanning', 'r').read()
+      # update those networks in the global state
+      update_networks(interface_networks, interface)
+    
+    # sleep until next update time
     time.sleep(discovery_sleep)
 
 # attempt to hack most promising network TODO
 def autohack_thread():
+  global networks
   while True:
-    # hack the most promising network TODO
+    # sleep until next update time
     time.sleep(autohack_sleep)
 
-# main function
+# ----- main function ----- #
 
 # start display thread
 threading.Thread(target=display_thread)
@@ -41,4 +60,3 @@ threading.Thread(target=autohack_thread)
 # stay alive
 while True:
   sleep(1)
-  
